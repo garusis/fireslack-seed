8527f7c5cc761a3accfc97588c0f4929fef8d95a
Marcos J. Alvarez <marcos.alvarez@rokk3rlabs.com>
2016-10-28 20:03:14 -0500

    Add basic state transition protection

--- app/app.js
+++ app/app.js
@@ -24,16 +24,45 @@ angular
         url: '/login',
         templateUrl: 'auth/login.html',
         controller: 'AuthController',
-        controllerAs: 'authCtrl'
+        controllerAs: 'authCtrl',
+        data: {
+          requireNoAuth: true
+        }
       })
       .state('register', {
         url: '/register',
         templateUrl: 'auth/register.html',
         controller: 'AuthController',
-        controllerAs: 'authCtrl'
+        controllerAs: 'authCtrl',
+        data: {
+          requireNoAuth: true
+        }
       });
 
     $urlRouterProvider.otherwise('/');
   })
+  .run(['$rootScope', 'Auth', '$state', function ($rootScope, Auth, $state) {
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+      var data = toState.data || {};
+      if (toParams.allowByAuth || (_.isNil(data.requireNoAuth) && _.isNil(data.requireAuth))) return;
+
+      event.preventDefault();
+      Auth.$requireAuth()
+        .then(function (auth) {
+          if (data.requireAuth) {
+            toParams.allowByAuth = true;
+            return $state.go(toState.name, toParams)
+          }
+          $state.go('home');
+        })
+        .catch(function (error) {
+          if (data.requireNoAuth) {
+            toParams.allowByAuth = true;
+            return $state.go(toState.name, toParams)
+          }
+          $state.go('login');
+        });
+    });
+  }])
   .constant('FirebaseUrl', 'https://fireslack-76c06.firebaseio.com/')
   .service('Firebase', Firebase);
